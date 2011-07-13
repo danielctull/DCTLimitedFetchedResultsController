bbc395bdb964513f4de90dc63962bf7ac27be3d8
Daniel Tull <dt@danieltull.co.uk>
2011-07-13 20:49:10 +0100

    Initial commit

---
+++ DCTLimitedFetchedResultsController.h
@@ -0,0 +1,13 @@
+//
+//  DCTLimitedFetchedResultsController.h
+//  DCTLimitedFetchedResultsController
+//
+//  Created by Daniel Tull on 13.07.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+
+
+@interface DCTLimitedFetchedResultsController : NSFetchedResultsController
+
+@end
---
+++ DCTLimitedFetchedResultsController.m
@@ -0,0 +1,23 @@
+//
+//  DCTLimitedFetchedResultsController.m
+//  DCTLimitedFetchedResultsController
+//
+//  Created by Daniel Tull on 13.07.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import "DCTLimitedFetchedResultsController.h"
+
+@implementation DCTLimitedFetchedResultsController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
